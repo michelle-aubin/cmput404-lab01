803165a1dd7fddc642ca5a22ba0ff8c4b19fa874
Michelle <phiangda@ualberta.ca>
2022-01-10 19:12:26 -0700

    modified

--- question2.py
+++ question2.py
@@ -2,4 +2,6 @@ import requests
 
 print("The version of the requests library installed on the system is", requests.__version__)
 
-print(requests.get("http://www.google.com/"))
+print("The source code of this file is: ")
+
+print(requests.get("https://raw.githubusercontent.com/michelle-aubin/cmput404-lab01/master/question2.py").text)
